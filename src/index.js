b82ce3f889e336ae527f3ec6f6ddfba26098cb1e
reygafitra <regafitra12@gmail.com>
2023-08-13 21:21:43 +0700

    add nasabah controller router

--- src/index.js
+++ src/index.js
@@ -10,8 +10,12 @@ const PORT = process.env.PORT
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Nasabah!')
 })
+
+const nasabahController = require('./controller/nasabah.controller')
+
+app.use('/nasabah', nasabahController)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
